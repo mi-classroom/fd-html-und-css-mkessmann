3e58cb452908b31ed7162458b93db587901e62d7
Marvin Dominik Kessmann <marvin_dominik.kessmann@smail.th-koeln.de>
2025-06-26 08:12:42 +0200

    Added function to turn strings into slugs, used for articleID (taken from Christian Noss)

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-/*Search and Menu Button interactions ###############################*/
+/*Search and Menu Button interactions #################################*/
 const menuOverlay = document.querySelector("[data-js-main-menu-target]");
 const menuOpenButton = document.querySelector("[data-js-main-menu-open]");
 const menuCloseButton = document.querySelector("[data-js-main-menu-close]");
@@ -15,8 +15,7 @@ searchOpenButton.addEventListener("click", function () {
   console.log("Searching...");
 });
 
-/*on page navigation generation ###############################*/
-
+/*on page navigation generation #######################################*/
 const pageNavigation = document.querySelector("[data-js-page-nav-menu]");
 const articleHeadlines = document.querySelectorAll(
   "[data-js-page-nav-content] > article > h2"
@@ -25,8 +24,9 @@ const articleHeadlines = document.querySelectorAll(
 articleHeadlines.forEach(function (headline) {
   const pageNavAnchor = document.createElement("a");
   const pageNavItem = document.createElement("li");
-  const articleID = headline.innerText;
+  let articleID = headline.innerText;
 
+  articleID = toSlug(articleID);
   headline.parentElement.setAttribute("id", articleID);
   pageNavAnchor.setAttribute("href", `#${articleID}`);
 
@@ -37,7 +37,28 @@ articleHeadlines.forEach(function (headline) {
   console.log(headline.parentElement.id);
 });
 
-/*Navigation on hover interaction ###############################*/
+/*turn string to slug (taken from Christian Noss' code) ###############*/
+function toSlug(text) {
+  return text
+    .toLowerCase()
+    .replace(/[äöüß]/g, (match) => {
+      switch (match) {
+        case "ä":
+          return "ae";
+        case "ö":
+          return "oe";
+        case "ü":
+          return "ue";
+        case "ß":
+          return "ss";
+      }
+    })
+    .replace(/[^a-z0-9]+/g, "-") // Replace non-alphanumeric characters with a single dash
+    .replace(/^-|-$/g, "") // Remove leading and trailing dashes
+    .replace(/--+/g, "-"); // Replace multiple dashes with a single dash
+}
+
+/*Navigation on hover interaction #####################################*/
 const navigationOnHover = document.querySelector(
   "[data-js-page-nav-menu] > li"
 );
